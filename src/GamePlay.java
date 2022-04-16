81788096a36903a9fd64542feeeb4497b72e00ca
Samuel Arguello <sam.rene12@gmail.com>
2022-04-16 10:13:04 -0500

    added color formatting

--- src/GamePlay.java
+++ src/GamePlay.java
@@ -27,7 +27,39 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
 
     public GamePlay(){
         map = new mapBrick (3,7);
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(true);
+        time = new Timer(delay, this);
+        time.start();
     }
+
+    public void paint(Graphics g){
+        //background
+        g.setColor(Color.black);
+        g.fillRect(1,1,692,592);
+        //draw
+        map.draw((Graphics2D)g);
+        //borders
+        g.setColor(Color.yellow);
+        g.fillRect(0,0,3,592);
+        g.fillRect(0,0,692,3);
+        g.fillRect(691, 0,3,592);
+        //scores
+        g.setColor(Color.white);
+        g.setFont(new Font("Verdana", Font.BOLD, 25));
+        g.drawString(""+score, 590, 30);
+        //paddle
+        g.setColor(Color.green);
+        g.fillRect(player1,550,100,8);
+        //ball
+        g.setColor(Color.yellow);
+        g.fillOval(ballposx, ballposy,20,20);
+    }
+
+
+
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
