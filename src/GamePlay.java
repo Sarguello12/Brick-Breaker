6e00512385e4c0776d16acf64c1e99a3bb7d296d
Samuel Arguello <sam.rene12@gmail.com>
2022-04-16 08:44:40 -0500

    completed initial setup of classes in Main and GamePlay

---
+++ src/GamePlay.java
@@ -0,0 +1,29 @@
+package src;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.Timer;
+import javax.swing.JPanel;
+
+
+
+public class GamePlay extends JPanel implements KeyListener, ActionListener {
+    private boolean play = false;
+    private int score = 0;
+    private int totalBricks = 21;
+    private Timer time;
+    private int delay = 5;
+    private int player1 = 310;
+    private int ballposx = 120;
+    private int ballposy = 350;
+    private int ballxdir = -1;
+    private int ballydir = -2;
+
+    private mapbrick map;
+}
